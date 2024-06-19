5f283d62723eb660799da8205d113b38e491b097
jreyesg18 <j.reyesg18@gmail.com>
2024-06-18 21:46:38 -0400

    modified files

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+  # Ruta raíz (puedes cambiarla a lo que prefieras)
+  root 'parametros#index'
+
+  # Rutas para el recurso `parametros`
   resources :parametros
-  # Otras rutas...
+
+  # Otras rutas de ejemplo
+  # get 'home/index'
+  # resources :users
+  # namespace :admin do
+  #   resources :products
+  # end
+
+  # Ruta por defecto si ninguna otra coincide
+  # get '*path' => redirect('/')
 end
